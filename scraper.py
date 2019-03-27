c72c8ad06f344e8da5970201a31e4660cd8de160
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-03-27 14:25:54 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,5 +2,5 @@ import scraperwiki
 import lxml.html
 html= scraperwiki.scrape("http://www.alva.org.uk/details.cfm?p=423")
 root = lxml.html.fromstring(html)
-museumtable=root.cssselect("div table tbody tr td")
+museumtable=root.cssselect("tbody tr td")
 print(museumtable)

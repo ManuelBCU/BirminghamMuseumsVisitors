8bead44340629a751fa11e91bd71a95a8e5deda6
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-03-27 14:13:34 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,14 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
+html= scraperwiki.scrape("http://www.alva.org.uk/details.cfm?p=423")
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+root=lxml.html.fromstring(html)
+museums=root.cssselect("div table tbody")
+record={}
+for museumtable in museums:
+         finallist=musemtable.text_content()
+         print(finallist.encode('utf-8'))
+         record['museumtable'] = finallist
+         
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+         scraperwiki.sqlite.save(['museumtable'],record)

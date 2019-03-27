4aaad463e031b1d99bcc170e66d01eddcf76181a
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-03-27 14:18:16 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,13 +2,4 @@ import scraperwiki
 import lxml.html
 html= scraperwiki.scrape("http://www.alva.org.uk/details.cfm?p=423")
 
-root=lxml.html.fromstring(html)
-museums=root.cssselect("div table tbody")
-record={}
-for museumtable in museums:
-         finallist=musemtable.text_content()
-         print(finallist.encode('utf-8'))
-         record['museumtable'] = finallist
-         
-
-         scraperwiki.sqlite.save(['museumtable'],record)
+print(html)
